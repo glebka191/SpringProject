e214af0d3d76844aa1da6c32072d3b95de19456e
GAntosik <glebka191@mail.ru>
2020-09-30 13:56:14 +0300

    тесты

--- src/main/java/com/innotech/controllers/BookingController.java
+++ src/main/java/com/innotech/controllers/BookingController.java
@@ -6,17 +6,19 @@ import com.innotech.entity.User;
 import com.innotech.repo.UserRepo;
 import com.innotech.service.BookingService;
 import com.innotech.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.Date;
 import java.util.Map;
 
 @Controller
 public class BookingController {
+
     private final BookingService bookingService;
     private final UserService userService;
     private final UserRepo userRepo;
@@ -36,21 +38,20 @@ public class BookingController {
     public String addBooking(Booking newBooking, @AuthenticationPrincipal User user,
                              SideTable sideTable, Date dateStart, Date dateEnd,
                              Map<String, Object> model) {
-        if (!bookingService.addBooking(newBooking)) {
-            model.put("message", "Уже есть бронь");
-            return "booking";
-        }
         newBooking.setDateStart(dateStart);
         newBooking.setDateEnd(dateEnd);
         newBooking.setUser(user);
         newBooking.setSideTable(sideTable);
-
+        if (!bookingService.addBooking(newBooking)) {
+            model.put("message", "Уже есть бронь");
+            return "booking";
+        }
+        bookingHistory(newBooking);
         return "booking";
     }
 
-    @Transactional
-    public Booking BookingHistory(Booking booking){
-        User user = userRepo.findByUsername(booking.getUser().getUsername());
-
+    @Transactional(propagation = Propagation.MANDATORY)
+    public void bookingHistory(Booking booking){
+        booking.getUser().getBookings().add(booking);
     }
 }
---
+++ src/test/java/com/innotech/controllers/BookingControllerTest.java
@@ -0,0 +1,16 @@
+package com.innotech.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookingControllerTest {
+
+    @Test
+    public void addBooking() {
+    }
+
+    @Test
+    public void bookingHistory() {
+    }
+}
